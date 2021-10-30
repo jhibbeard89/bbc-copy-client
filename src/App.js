c99e2d32d6f1f61a8fe92188ccd47e385efe486d
James Hibbeard <jhibbeard@gmail.com>
2021-10-30 16:52:25 +0200

    Revert "Connect to the API"

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import ArticleBox from "./components/ArticleBox";
 
 function App() {
-  const [articles, setArticles] = useState();
-
-  // const [title, setTitle] = useState(
-  //   "Climate and Covid top agenda as world leaders meet"
-  // );
-
-  // const [subHeader, setSubHeader] = useState(
-  //   "The G20 leaders are holding tehri first face-to-face meeting since the start of the pandemic"
-  // );
-
-  // const [imageLink, setImageLink] = useState(
-  //   "https://ichef.bbci.co.uk/news/976/cpsprodpb/13CA1/production/_121275018_996a9b7e-6a23-4ff6-be7f-79db818e8600.jpg"
-  // );
-
-  // const [tag, setTag] = useState("WORLD");
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api/v1/articles")
-      .then((response) => response.json())
-      .then((data) => {
-        setArticles(data);
-      });
-  }, []);
-
-  // console.log(articles);
-  articles.map((article) => console.log(article.title));
+  const title = "Climate and Covid top agenda as world leaders meet";
+  const subHeader =
+    "The G20 leaders are holding tehri first face-to-face meeting since the start of the pandemic";
+  const imageLink =
+    "https://ichef.bbci.co.uk/news/976/cpsprodpb/13CA1/production/_121275018_996a9b7e-6a23-4ff6-be7f-79db818e8600.jpg";
+  const tag = "WORLD";
 
   return (
     <div className="App">
-      {articles.map((article) => (
-        <ArticleBox
-          title={article.title}
-          subHeader={article.subTitle}
-          imageLink={article.imageLink}
-          tag="WORLD"
-        />
-      ))}
+      <ArticleBox
+        title={title}
+        subHeader={subHeader}
+        imageLink={imageLink}
+        tag={tag}
+      />
     </div>
   );
 }
